f7ddf4f51581d29a947e5a6a84d6d8431ca78596
Moe Coovadia <coovadia.moe@gmail.com>
2024-02-15 16:58:08 -0500

    Added tableau work

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,7 +17,7 @@ const Home = () => {
         <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
           <p className="text-pink-600 sm:text-4xl">Hi, my name is</p>
           <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
-            Muhammed Coovadia
+            Moe Coovadia
           </h1>
           <h2 className="text-4xl sm:text-7xl font-bold text-[#000000]">
             I'm a Full Stack Developer.
--- src/components/Work.js
+++ src/components/Work.js
@@ -2,6 +2,7 @@ import React from "react";
 import Todo from "../assets/ToDo.jpg";
 import Calculator from "../assets/Calculator.jpg";
 import Typing from "../assets/Typing.jpg";
+import Tableau from "../assets/Tableau.PNG";
 
 const Work = () => {
   return (
@@ -19,6 +20,24 @@ const Work = () => {
         {/* Container */}
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${Tableau})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                SuperStore Data
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://public.tableau.com/views/SuperStoreData_17078704325310/Overview?:language=en-US&:sid=&:display_count=n&:origin=viz_share_link">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
           <div
             style={{ backgroundImage: `url(${Typing})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
